d472c8f3ff8b1ee3841c7cdf16c87b92fea72e71
Im-zeus <87465532+Im-zeus@users.noreply.github.com>
2021-07-25 17:36:49 +0530

    Create private.py

--- Group_Music_Probot/modules/private.py
+++ Group_Music_Probot/modules/private.py
@@ -14,7 +14,7 @@ async def start(_, message: Message):
     await message.reply_text(
         f"""**Hey, I'm {BOT_NAME} 🎵
         
-I am Group Music Probot, I Am an Advance And Powerful Telegram Groups Voice Chat Music Bot.
+Im Parvathy, an Advance And Powerful Telegram Groups Voice Chat Music Bot.
 
 Try the /help Command below to know my abilities.
         """,
